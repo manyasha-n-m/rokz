ec833f84ea42fa26afff5236a2de6bd74e630b37
manyasha-n-m <natasha.monina@gmail.com>
2021-10-10 21:02:51 +0300

    added ellipse restriction

--- lab2/perceptrone.py
+++ lab2/perceptrone.py
@@ -7,7 +7,7 @@ def init():
     with open('lab2/train_01.json') as f:
         d1 = json.load(f)
     d = []
-    r = {'inside':1, 'outside':-1}
+    r = {'inside':-1, 'outside':1}
     for k in d1.keys():
         _df = pd.DataFrame(d1[k], columns=['A', 'B'])
         _df['class'] = r[k]
@@ -25,7 +25,7 @@ def plot(df):
     font = {'weight' : 'bold', 'size'   : 15}
     plt.rc('font', **font)
     try:
-        df1, df2 = [x for _, x in df.groupby(df['class'] == -1)]
+        df1, df2 = [x for _, x in df.groupby(df['class'] == 1)]
         plt.scatter(df1.A, df1.B)
         plt.scatter(df2.A, df2.B)
     except Exception:
@@ -51,14 +51,29 @@ class Classifier:
         self.X = self.df[['A', 'B']]
         self.X['A2'] = self.X.A*self.X.A
         self.X['B2'] = self.X.B*self.X.B
-        self.X['AB'] = self.X.A*self.X.B
+        self.X['2AB'] = 2*self.X.A*self.X.B
         self.X['C'] = 1
         self.y = train['class']
         self.w = np.zeros(6)
+        self.add_eigen(True)
     
+    def add_eigen(self, first = False):
+        w = np.asarray(self.w)
+        self.sigma = np.array([[w[2], w[4]], [w[4], w[3]]])
+        _, vectors = np.linalg.eig(self.sigma)
+        eig = []
+        for v in vectors.T:
+            eig.append([0,0, v[0]**2, v[1]**2, 2*v[1]*v[0], 0])
+        if first:
+            self.X = self.X.append(pd.DataFrame(eig, columns=['A', 'B', 'A2','B2','2AB','C']), ignore_index=True)
+            self.y = np.append(self.y, np.ones(2))
+        else:
+            self.X = self.X.iloc[:-2,:].append(pd.DataFrame(eig, columns=['A', 'B', 'A2','B2','2AB','C']), ignore_index=True)
+            self.y = np.append(self.y[:-2], np.ones(2))    
+            
     def fix(self, X, y):
-        self.w += y*X/np.linalg.norm(X)
-        
+        self.w += y * X / np.linalg.norm(X)
+                      
     def check(self):
         res = self.y * np.sum(self.w * self.X, axis=1)
         bad = self.X[res <= 0]
@@ -71,6 +86,7 @@ class Classifier:
         flag, idx = self.check()
         while not flag:
             self.fix(self.X.loc[idx,:], self.y[idx])
+            self.add_eigen()
             flag, idx = self.check()
         print(self.w)
     
@@ -82,9 +98,9 @@ class Classifier:
         test = df[['A', 'B']]
         test['A2'] = test.A*test.A
         test['B2'] = test.B*test.B
-        test['AB'] = test.A*test.B
+        test['2AB'] = 2*test.A*test.B
         test['C'] = 1
-        test['class_detected'] = (np.sum(self.w * test[['A', 'B', 'A2','B2','AB','C']], axis=1) > 0).astype(int)
+        test['class_detected'] = (np.sum(self.w * test[['A', 'B', 'A2','B2','2AB','C']], axis=1) > 0).astype(int)
         test['class_detected'].replace(0,-1,inplace=True)
         return test
     
@@ -97,6 +113,7 @@ class Classifier:
         X, Y = np.meshgrid(x,y)
         ones = np.ones((100,100))
         Z = np.zeros((100,100))
-        for i, m in enumerate([X,Y,X*X,Y*Y, X*Y, ones]):
+        for i, m in enumerate([X,Y,X*X,Y*Y, 2*X*Y, ones]):
             Z+=self.w[i]*m
         plt.contour(X, Y, Z, [0])
+
