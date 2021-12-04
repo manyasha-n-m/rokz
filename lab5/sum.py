780cf830067725034692b403d9a2964a5575b439
manyasha-n-m <natasha.monina@gmail.com>
2021-12-05 00:42:34 +0200

    q defined

--- lab5/sum.py
+++ lab5/sum.py
@@ -8,15 +8,17 @@ images = []
 masks = []
 for i in range(1,6):
     images.append(cv2.imread(im.format(i)))
-    masks.append(cv2.imread(mask.format(i), cv2.IMREAD_GRAYSCALE))
+masks.append(cv2.imread(mask.format(i), cv2.IMREAD_GRAYSCALE))
 
 
 class union:
-    def __init__(self, pics, masks):
-        self.x = pics
-        self.b = masks
+    def __init__(self, pics, masks, a=1, b=1):
+        self.x = np.array(pics)
+        self.b = np.array(masks, dtype=bool)
         self.h, self.w = self.b[0].shape
         self.P = np.zeros((self.h, self.w))
+        self.q = a*(~self.b)
+
 
 
 if __name__ == "__main__":
