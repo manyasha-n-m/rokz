4fa9297b86f09aafa526b16a462ebef7bd49da47
manyasha-n-m <natasha.monina@gmail.com>
2021-12-05 02:30:59 +0200

    finished g, not tested

--- lab5/sum.py
+++ lab5/sum.py
@@ -8,17 +8,19 @@ images = []
 masks = []
 for i in range(1,6):
     images.append(cv2.imread(im.format(i)))
-masks.append(cv2.imread(mask.format(i), cv2.IMREAD_GRAYSCALE))
+    masks.append(cv2.imread(mask.format(i), cv2.IMREAD_GRAYSCALE))
 
 
 class Union:
-    def __init__(self, pics, masks, a=1, b=1):
+    def __init__(self, pics, masks, alpha=1, beta=1):
         self.x = np.array(pics)
         self.m = self.x.shape[0]
         self.b = np.array(masks, dtype=bool)
         self.h, self.w = self.b[0].shape
         self.P = np.zeros((self.h, self.w))
-        self.q = a*(~self.b)
+        self.alpha, self.beta = alpha, beta
+        self.q = alpha*(~self.b)
+        self.g = self.create_g()
 
     def create_g(self):
         norm = {}
@@ -30,10 +32,19 @@ class Union:
                             np.abs(self.x[i, :, :, 2]-self.x[j, :, :, 2])
                 norm[i, j] = norm[j, i] = _norm_kk_
         norm[self.m-1, self.m-1] = np.zeros((self.h, self.w))
-        
-        for i in range(self.h):
-            for j in range(1, self.w):
-                pass
+
+        g = []
+        for row in range(self.h):
+            g_row = {}
+            for col in range(1, self.w):
+                g_row[col-1, col] = np.zeros((self.m, self.m))
+                for i in range(self.m-1):
+                    for j in range(i, self.m):
+                        g_row[col-1, col][i, j] = g_row[col-1, col][i, j] = self.beta*(norm[i, j][row, col-1] +\
+                                                                                       norm[i, j][row, col])
+            g.append(g_row)
+        return g
+
 
 if __name__ == "__main__":
     import doctest
