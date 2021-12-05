f75a1e23ac7d829d58bc5f6fa1b75ea90b8e2711
manyasha-n-m <natasha.monina@gmail.com>
2021-12-05 23:41:14 +0200

    fixed add fj

--- lab5/sum.py
+++ lab5/sum.py
@@ -3,8 +3,8 @@ st = time.time()
 import numpy as np
 import cv2
 
-im_path = 'data1\\0{}.jpg'
-mask_path = 'data1\\m_0{}.png'
+im_path = 'data\\image_0{}.png'
+mask_path = 'data\\mask_0{}.png'
 if not __name__ == "__main__":
     im_path = 'lab5/data/image_0{}.png'
     mask_path = 'lab5/data/mask_0{}.png'
@@ -65,7 +65,7 @@ class Union:
         K = np.zeros((self.w, self.h), dtype=int)
         K[0, :] = np.argmin(f[0, :, :], axis=0)
         for j in range(1, self.w):
-            _table = self.q[:, :, j] + self.g_optimal(K[j-1,:], j-1) + f[j, :, :]
+            _table = self.q[:, :, j] + self.g_optimal(K[j-1, :], j-1) + f[j+1, :, :]
             K[j, :] = np.argmin(_table, axis=0)
         return K
 
@@ -80,9 +80,9 @@ class Union:
 im, m = read(im_path, mask_path)
 u = Union(im, m, 255*6)
 res = u.merge()
-cv2.imwrite('8.png', res)
+cv2.imwrite('12.png', res)
 print(time.time()-st)
 
 if __name__ == "__main__":
     import doctest
-    doctest.testmod(verbose=True)
+    doctest.testmod()
