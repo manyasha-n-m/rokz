40f4f728942fa8bbf4f29d4da8f37c6e71b71dce
manyasha-n-m <natasha.monina@gmail.com>
2021-12-27 19:09:49 +0200

    base class+ipynb for testing defferent samples created

--- lab6/lab.py
+++ lab6/lab.py
@@ -1,1 +1,23 @@
-print('1')
+import numpy as np
+import cv2
+
+
+class BinSum:
+    def __init__(self, img, e0, e1):
+        self.img = img
+        self.e0 = e0
+        self.e1 = e1
+        self.h, self.w = e0.shape
+        self.gh = self.create_gh()
+        self.gv = self.create_gv()
+        self.g = self.create_g()
+
+    def create_gh(self):
+        pass
+
+    def create_gv(self):
+        pass
+
+    def create_g(self):
+        pass
+    
